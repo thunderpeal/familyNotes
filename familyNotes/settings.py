6513f8e0778378f26391c60a659da1b702b1c66d
vyacheslav <pealsgun@gmail.com>
2021-08-26 02:39:02 +0300

    secret key changed and hidden
    requirements set

--- familyNotes/settings.py
+++ familyNotes/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pf&jir_d4_28$sdwhv!$&%!8q8b-f4y_@!7x2y7wlfpn0g9c7-'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
